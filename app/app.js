f1b480327689a5e56b53f1603cbd495eee040226
Prajwol Gyawali <prajwol.lucifer@gmail.com>
2020-02-12 11:40:35 +0545

    Fixed the ability to export pdf and some redundant codes removed.

--- app/app.js
+++ app/app.js
@@ -2,41 +2,41 @@ const electron = require('electron');
 const path = require('path');
 const url = require('url');
 process.env.NODE_ENV = 'development';
-const {app, BrowserWindow, Menu, ipcMain} = electron;
+const { app, BrowserWindow, Menu, ipcMain } = electron;
 const fs = require('fs');
 const os = require('os');
-const {shell} = require('electron') // deconstructing assignment
+const { shell } = require('electron') // deconstructing assignment
 const dir_prefix_name = app.getAppPath();
 const pdf_path = dir_prefix_name + "/pdf_dir/";
 const word_path = dir_prefix_name + "/word_dir/";
-let db='';
+let db = '';
 let mainWindow;
 let addWindow;
 let Table_name = "";
 
 
-app.on('window-all-closed', function() {
-    app.quit();
+app.on('window-all-closed', function () {
+  app.quit();
 });
 
 // This method will be called when Electron has done everything
 // initialization and ready for creating browser windows.
-app.on('ready', function() {
+app.on('ready', function () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1000, height: 720, webPreferences: {nodeIntegration:true}});
-    mainWindow.loadURL(url.format({
+  mainWindow = new BrowserWindow({ width: 1000, height: 720, webPreferences: { nodeIntegration: true } });
+  mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'menu.html'),
     protocol: 'file:',
-    slashes:true
+    slashes: true
   }));
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
+  mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     mainWindow = null;
-	  app.quit();
+    app.quit();
   });
 
   /*
@@ -48,65 +48,113 @@ app.on('ready', function() {
 });
 
 // Handle add item window
-function createWindow(TITLE,HTML_FILE){
+function createWindow(TITLE, HTML_FILE) {
   addWindow = new BrowserWindow({
-    show:false,
+    show: false,
     width: 1000,
-    height:720,
-    title:TITLE,
+    height: 720,
+    title: TITLE,
     //titleBarStyle: 'default',
     parent: mainWindow,
     webPreferences: {
       nodeIntegration: true
     }
   });
-	addWindow.once('ready-to-show', () => {
-		addWindow.show()
-	});
+  addWindow.once('ready-to-show', () => {
+    addWindow.show()
+  });
   addWindow.loadURL(url.format({
     pathname: path.join(__dirname, HTML_FILE),
     protocol: 'file:',
-    slashes:true
+    slashes: true
   }));
 
   //const menu = new Menu();
   // Handle garbage collection
 
-  addWindow.on('close', function(){
+  addWindow.on('close', function () {
     addWindow = null;
   });
   return addWindow;
 }
 
 
+//EDIT FROM PRASHANT
+//Button clicks handler.
+//Every button in the program sends a "button clicked" event and ID as the parameter.
+//some clicks might not have functionality.
+
+ipcMain.on('buttonClicked', function (e, buttonId) {
+  if (buttonId === "newRoutine") {
+    createWindow("New Routine", "RoutineName.html");
+    console.log('new routine');
+    // mainWindow.webContents.send("dbName",db_name ,dir_prefix_name);
+  }
+
+  else if (buttonId == "oldRoutine") {
+    createWindow("old Routine", "oldRoutine.html");
+    console.log("update old routine");
+  }
+
+  else if (buttonId == "editTeacher") {
+    createWindow("Edit Teacher", "editTeacher.html");
+    console.log('edit teacher');
+  }
+
+  else if (buttonId == "editCourse") {
+    createWindow("Edit Course", "editSubject.html");
+    console.log('edit course');
+  }
+
+  else if (buttonId == "addTeacher") {
+    createWindow("Add Teacher", "addTeacher.html");
+    console.log('add teacher');
+  }
+
+  else if (buttonId == "addCourse") {
+    createWindow("Add Course", "addSubject.html");
+    console.log('add course');
+  }
+  else if (buttonId == "addSupervisor") {
+    createWindow("Add Supervisor", "addSupervisor.html");
+  }
+  else if (buttonId == "editSupervisor") {
+    createWindow("Edit Supervisor", "Edit_supervisor.html");
+  }
+
+
+  // else if(buttonId=="saveRoutine"){
+  //   createWindow("Name of Routine", "databaseName.html");
+  // }
+});
 // Catch item:add
-ipcMain.on('Teacher:add', function(e, teacher_name, teacher_initials){
-  mainWindow.webContents.send('Teacher:Store',teacher_name, teacher_initials);
+ipcMain.on('Teacher:add', function (e, teacher_name, teacher_initials) {
+  mainWindow.webContents.send('Teacher:Store', teacher_name, teacher_initials);
   addWindow.close();
   // Still have a reference to addWindow in memory. Need to reclaim memory (Grabage collection)
   //addWindow = null;
 });
 
-ipcMain.on('Supervisor:add', function(e, Supervisor_name, Supervisor_initials, Supervisor_position){
+ipcMain.on('Supervisor:add', function (e, Supervisor_name, Supervisor_initials, Supervisor_position) {
   // console.log(Supervisor_name);
   // console.log(Supervisor_initials);
   // console.log(Supervisor_position);
-  mainWindow.webContents.send('Supervisor:Store',Supervisor_name, Supervisor_initials,Supervisor_position);
+  mainWindow.webContents.send('Supervisor:Store', Supervisor_name, Supervisor_initials, Supervisor_position);
   addWindow.close();
   // Still have a reference to addWindow in memory. Need to reclaim memory (Grabage collection)
   //addWindow = null;
 });
 
 
-ipcMain.on('Subject:add', function(e, Subject_name){
-  mainWindow.webContents.send('Subject:Store',Subject_name);
+ipcMain.on('Subject:add', function (e, Subject_name) {
+  mainWindow.webContents.send('Subject:Store', Subject_name);
   addWindow.close();
   // Still have a reference to addWindow in memory. Need to reclaim memory (Grabage collection)
   //addWindow = null;
 });
 
 
-ipcMain.on('Database:add', function(e, item){
+ipcMain.on('Database:add', function (e, item) {
   console.log(item);
   mainWindow.webContents.send('DataBase:Create', item);
   addWindow.close();
@@ -115,17 +163,17 @@ ipcMain.on('Database:add', function(e, item){
   //addWindow = null;
 });
 
-ipcMain.on('Routine:add', function(e, Routine_name, batch_yr, prog_full, prog_acr, year_rot, yr_part){
+ipcMain.on('Routine:add', function (e, Routine_name, batch_yr, prog_full, prog_acr, year_rot, yr_part) {
   Table_name = Routine_name;
-  console.log(Table_name);    
-  mainWindow.webContents.send('Routine:Create', Table_name,batch_yr, prog_full, prog_acr, year_rot, yr_part);
+  console.log(Table_name);
+  mainWindow.webContents.send('Routine:Create', Table_name, batch_yr, prog_full, prog_acr, year_rot, yr_part);
   addWindow.close();
   // Still have a reference to addWindow in memory. Need to reclaim memory (Grabage collection)
   //addWindow = null;
 });
 
 
-ipcMain.on('Routine:old', function(e, Routine_name){
+ipcMain.on('Routine:old', function (e, Routine_name) {
   Table_name = Routine_name;
   console.log(Table_name);
   mainWindow.webContents.send('Routine:append', Table_name);
@@ -135,94 +183,43 @@ ipcMain.on('Routine:old', function(e, Routine_name){
 });
 
 
-//EDIT FROM PRASHANT
-//Button clicks handler.
-//Every button in the program sends a "button clicked" event and ID as the parameter.
-//some clicks might not have functionality.
-
-ipcMain.on('buttonClicked', function(e, buttonId){
-  if(buttonId==="newRoutine"){
-    createWindow("New Routine","RoutineName.html");
-    console.log('new routine');
-    // mainWindow.webContents.send("dbName",db_name ,dir_prefix_name);
-  }
-  
-  else if(buttonId=="oldRoutine"){
-    createWindow("old Routine","oldRoutine.html");
-    console.log("update old routine");
-  }
-  
-  else if(buttonId == "editTeacher"){
-    createWindow("Edit Teacher","editTeacher.html");
-    console.log('edit teacher');
-  }
-
-  else if(buttonId=="editCourse"){
-    createWindow("Edit Course","editSubject.html");
-    console.log('edit course');
-  }
-
-  else if(buttonId == "addTeacher"){
-    createWindow("Add Teacher","addTeacher.html");
-    console.log('add teacher');
-  }
-
-  else if(buttonId=="addCourse"){
-    createWindow("Add Course","addSubject.html");
-    console.log('add course');
-  }
-  else if(buttonId=="addSupervisor"){
-    createWindow("Add Supervisor","addSupervisor.html");
-  }
-  else if(buttonId=="editSupervisor"){
-    createWindow("Edit Supervisor","Edit_supervisor.html");
-  }
-
-
-  // else if(buttonId=="saveRoutine"){
-  //   createWindow("Name of Routine", "databaseName.html");
-  // }
-});
 
 
 // pdf print function
 //closing the window by "cancel" button.
-ipcMain.on('closeWindow', function(e){
+ipcMain.on('closeWindow', function (e) {
   addWindow.close();
 });
 
-ipcMain.on('hideWindow', function(e){
+ipcMain.on('hideWindow', function (e) {
   addWindow.hide();
 });
 
-ipcMain.on('print-to-pdf', function(event, table_name){
-  table_name_ext = table_name + '.pdf'
-  const pdfPath = path.join(pdf_path, table_name_ext);
-  console.log(pdfPath);
-  const win = BrowserWindow.fromWebContents(event.sender);
-
-  win.webContents.printToPDF({}, (error, data) => {
-    if (error) return console.log(error.message);
-
-    fs.writeFile(pdfPath, data, err => {
-      if (err) return console.log(err.message);
-      shell.openExternal('file://' + pdfPath);
-      event.sender.send('wrote-pdf', pdfPath);
+//Saves the pdf without any dailog.
+ipcMain.on('print-to-pdf', (event, arg) => {
+  routine_name_ext = arg + '.pdf'
+  const pdfPath = path.join(pdf_path, routine_name_ext);
+  mainWindow.webContents.printToPDF({
+    pageSize: 'Letter'
+  }).then(data => {
+    fs.writeFileSync(pdfPath, data, (err) => {
+      if (err) throw err
+      console.log('PDF success!')
     })
-    
-  })
+  }
+  )
 });
 
-// Receiving communication from index.html for new window creation and passing table_name info to main process..ie..app.js
 
-ipcMain.on('print-to-Word', function(event, table_name) {
-      Table_name = table_name;
-      secondWindow = createWindow("Word","printable.html");  
+// Receiving communication from index.html for new window creation and passing table_name info to main process..ie..app.js
+ipcMain.on('print-to-Word', function (event, table_name) {
+  Table_name = table_name;
+  secondWindow = createWindow("Word", "printable.html");
 });
 
 //receiving communication from printable.html for quering Routine_name information
 
-ipcMain.on('print-to-Word2',function(e){
+ipcMain.on('print-to-Word2', function (e) {
   console.log("yaay");
   console.log(Table_name);
   secondWindow.webContents.send('doc_name', Table_name);
